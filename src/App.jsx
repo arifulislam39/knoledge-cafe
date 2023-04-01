4a1cd875a8e19297bb063515725713615d331512
ariful-islam <arifulislam204909@gmail.com>
2023-04-01 16:56:00 +0600

    web logo added

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,14 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import Logo from './assets/icon.png'
 import './App.css'
 import Header from './Components/Header/Header'
 import Display from './Components/Display/Display';
-import Cart from './Components/Cart/Cart'
-import Blog from './Components/Blog/Blog'
-import ReadTime from './Components/ReadTime/ReadTime'
+// import Cart from './Components/Cart/Cart'
+// import Blog from './Components/Blog/Blog'
+// import ReadTime from './Components/ReadTime/ReadTime'
 import FAQ from './Components/FAQ/FAQ'
-import { ToastContainer, toast } from 'react-toastify';
-  import 'react-toastify/dist/ReactToastify.css';
+// import { ToastContainer, toast } from 'react-toastify';
+//   import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
   //const [count, setCount] = useState(0)
